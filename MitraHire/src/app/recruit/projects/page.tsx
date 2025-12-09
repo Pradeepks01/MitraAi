51687c2e36030e7a57d97ebbfc69321bd22e2733
Pradeepks01 <pradeepks7483@gmail.com>
2025-12-09 20:17:04 +0530

    Wrap projects page in Suspense

--- MitraHire/src/app/recruit/projects/page.tsx
+++ MitraHire/src/app/recruit/projects/page.tsx
@@ -1,11 +1,11 @@
 import RecruiterProjects from '@/components/Recruiter/RecruiterProjects'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
     return (
-        <div>
+        <Suspense fallback={<div className="flex justify-center p-8">Loading projects...</div>}>
             <RecruiterProjects />
-        </div>
+        </Suspense>
     )
 }
 
