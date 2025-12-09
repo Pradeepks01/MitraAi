1ee8f6a91b6551c9498a5670e7f52b2f9c164300
Pradeepks01 <pradeepks7483@gmail.com>
2025-12-09 22:57:20 +0530

    Update Firebase credentials

--- MitraHire/src/components/firebase/firebaseConfig.js
+++ MitraHire/src/components/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBQJyTvILxge7C9zwLv7jKGGJh0n1yoCu8",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mitraai-42c5a.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mitraai-42c5a",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mitraai-42c5a.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "345717350669",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:345717350669:web:6f470c113179e833965044"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyD6z3BySbqv8l4nbingcfKAV4-6b8SepUc",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "mitraai-9c61d.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mitraai-9c61d",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "mitraai-9c61d.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "981481213460",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:981481213460:web:4674d3a4e21761ab6a2101"
 };
 
 
